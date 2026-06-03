1b10426165561d6ba8cbf33cbc4a6fe2f3815aae
echo-syn <117386174+Bronyte@users.noreply.github.com>
2026-06-03 22:16:13 +0200

    Add files via upload
    API demo

---
+++ models.py
@@ -0,0 +1,58 @@
+from db import Database
+
+class User(Database.Model):
+
+    __tablename__ = 'users'
+
+    id = Database.Column(Database.Integer, primary_key=True)
+    username = Database.Column(Database.String(80), unique=True, nullable=False)
+    role = Database.Column(Database.String(80), nullable=False)
+
+    def to_dict(self):
+        return {
+                    "id": self.id,
+                    "username": self.username,
+                    "role": self.role
+                }
+
+
+class Trucks(Database.Model):
+
+    __tablename__ = 'trucks'
+
+    id = Database.Column(Database.Integer, primary_key=True)
+    name = Database.Column(Database.String(80), nullable=False)
+    registration_number = Database.Column(Database.String(80), unique=True, nullable=False)
+    status = Database.Column(Database.String(80), nullable=False)
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "registration_number": self.registration_number,
+            "status": self.status
+        }
+
+class Deliveries(Database.Model):
+    __tablename__ = 'deliveries'
+
+    id = Database.Column(Database.Integer, primary_key=True)
+    origin = Database.Column(Database.String(80), nullable=False)
+    destination = Database.Column(Database.String(80), nullable=False)
+    weight = Database.Column(Database.Integer, nullable=False)
+    assigned_truck_id = Database.Column(Database.Integer, Database.ForeignKey('trucks.id'), nullable=False)
+    status = Database.Column(Database.String(80), nullable=False)
+
+    truck = Database.relationship('Trucks', backref=Database.backref('deliveries', lazy=True), foreign_keys=[assigned_truck_id])
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "registration_number": self.registration_number,
+            "origin": self.origin,
+            "destination": self.destination,
+            "weight": self.weight,
+            "truck": self.assigned_truck_id,
+            "status": self.status
+        }
